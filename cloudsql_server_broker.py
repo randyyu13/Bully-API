d95ae0f1743f00ff8c9aeb880d845fe2e31be7fa
randyyu13 <ldyu999@gmail.com>
2024-04-20 15:51:41 -0500

    broker updates

--- cloudsql_server_broker.py
+++ cloudsql_server_broker.py
@@ -1,12 +1,13 @@
 from google.cloud.sql.connector import Connector
 import sqlalchemy
 
-class cloudsql_broker:
+class cloudsql_server_broker:
     def __init__(self, project_id, region, instance_name, user, pswd, db_name):
         self.__instance_connection_name = f"{project_id}:{region}:{instance_name}"
         self.__db_user = user
         self.__db_pass = pswd
         self.__db_name = db_name
+        self.__engine = self.connect_with_connector()
 
     def connect_with_connector(self) -> sqlalchemy.engine.base.Engine:
         """
@@ -37,3 +38,16 @@ class cloudsql_broker:
             creator=getconn,
         )
         return pool
+
+    def execute_query(self, query: str):
+        """
+        Executes a SQL query on the connected database.
+
+        Args:
+            query (str): The SQL query to be executed.
+        Returns:
+            sqlalchemy.engine.result.ResultProxy: The result of the query.
+        """
+        with self.__engine.connect() as conn:
+            result = conn.execute(query)
+        return result
